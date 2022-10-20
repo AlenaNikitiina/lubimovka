2c1659d1c4f63ac9072ba7604e84d48c848d719c
Alena Nikitina <alena.tkachenk@mail.ru>
2022-10-20 14:49:18 +0300

    feat: добавила составные части попапа

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,39 @@
-const popuup = document.querySelector('popup')
+const popup = document.querySelector('.popup');
+const popupZoom = document.querySelector('.popup__zoom');
+const popupCloseButton = document.querySelectorAll('.popup__close-button'); // кнопка закрыть попап, крестик
+
+//zoom попап
+const popupImage = document.querySelector('.popup__image');
+
+
+// функция открытия попапа
+function openPopup (item) {
+  item.classList.add('popup_opened');
+  document.addEventListener('keydown', closeByEscape);
+}
+
+// функция закрытия попапа
+function closePopup (item) {
+  item.classList.remove('popup_opened');
+  document.removeEventListener('keydown', closeByEscape);
+}
+
+// функция закрытия попапов по нажатию на Escape
+const closeByEscape = (evt) => {
+  if (evt.key === 'Escape') {
+    const openedNowPopup = document.querySelector('.popup_opened')
+    closePopup(openedNowPopup);
+  }
+}
+
+/*
+// закрыть попапы нажав на оверлей или крестик
+popup.forEach((item) => {
+  item.addEventListener('mousedown', (evt) => {
+    if (evt.target.classList.contains('popup_opened') || evt.target.classList.contains('popup__close-button')) {
+      closePopup(item);
+    }
+  })
+})
+
+*/
