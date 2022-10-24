5eb482db75426f9a62758cbebb99900bd16e255e
Evgeny Gridin <jon666gridin@yandex.ru>
2022-10-24 15:08:50 +0200

    finish show + adaptive

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+let offfset = 0;
+const contentLine = document.querySelector('.show__content');
+
+document.querySelector('.show__next').addEventListener('click', function(){
+  offfset = offfset + 270;
+  if (offfset > 1050) {
+    offfset = 0;
+  }
+  contentLine.style.left = -offfset + 'px';
+});
+
+document.querySelector('.show__prev').addEventListener('click', function(){
+  offfset = offfset - 270;
+  if (offfset < 0) {
+    offfset = 1050;
+  }
+  contentLine.style.left = -offfset + 'px';
+});
+
