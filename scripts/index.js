3868fbc8d5d47716ebd68588f09031e96113caf1
Alena Nikitina <alena.tkachenk@mail.ru>
2022-10-24 22:43:51 +0300

    working

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,33 @@
-const popup = document.querySelector('.popup');
-const popupZoom = document.querySelector('.popup__zoom');
+const popup = document.querySelector('.popup'); // нашли попап
+const popupZoom = document.querySelector('.popup_zoom'); // второй класс попапа
 const popupCloseButton = document.querySelectorAll('.popup__close-button'); // кнопка закрыть попап, крестик
+const popupImage = document.querySelector('.popup__img'); // нашли фото из попапа
 
-//zoom попап
-const popupImage = document.querySelector('.popup__image');
 
 
 // функция открытия попапа
 function openPopup (item) {
   item.classList.add('popup_opened');
-  document.addEventListener('keydown', closeByEscape);
+  //document.addEventListener('keydown', closeByEscape);
 }
 
 // функция закрытия попапа
 function closePopup (item) {
   item.classList.remove('popup_opened');
-  document.removeEventListener('keydown', closeByEscape);
+  //document.removeEventListener('keydown', closeByEscape);
 }
 
+popup.addEventListener('click', openPopup())
+
+/*
+popupZoom.addEventListener('click', function (evt) {
+  //const a = document.querySelector('.element__foto');
+  openPopup(popupZoom)
+})
+
+
+
+/*
 // функция закрытия попапов по нажатию на Escape
 const closeByEscape = (evt) => {
   if (evt.key === 'Escape') {
